1ea700e4c19be4a939b287e1aefb1bc99f0453df
geraldokutar <geraldpilk@gmail.com>
2018-07-24 11:57:21 +0300

    Copying code from JSfiddle to script.js file

---
+++ js/script.js
@@ -0,0 +1,7 @@
+var add = function(number1, number2) {
+return number1 + number2;
+};
+
+var number1 = parseInt(prompt("What is your number:"))
+var number2 = parseInt(prompt("what is your second number:"))
+alert(add(number1, number2));
