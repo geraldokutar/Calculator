644a10f5e23e67d172bae6967d20e9099e392c82
geraldokutar <geraldpilk@gmail.com>
2018-07-24 12:30:15 +0300

    Add a multipliction function

--- js/script.js
+++ js/script.js
@@ -14,4 +14,12 @@ var subtract = function(number1, number2) {
 var number1 = parseInt(prompt("what is your first number:"))
 var number2 = parseInt(prompt("what is your second number"))
 var result = subtract(number1, number2);
-alert(result);
+alert(result)
+alert("multiplication")
+var multiply = function(number1, number2){
+  return number1 + number2;
+};
+var number1 = parseInt(prompt("what is your first number:"))
+var number2 = parseInt(prompt("what is your second number:"))
+var result = multiply(number1, number2);
+alert(result)
