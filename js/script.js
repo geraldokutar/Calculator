2ca84cddc6e42a3f23e224afb6b6a7b3d85ba40d
geraldokutar <geraldpilk@gmail.com>
2018-07-29 11:37:54 +0300

    Add js rule

--- js/script.js
+++ js/script.js
@@ -1,33 +1,15 @@
-alert("Addition")
 var add = function(number1, number2) {
-  return number1 + number2;
-};
+      return number1 + number2;
+    };
 
-var number1 = parseInt(prompt("What is your number:"))
-var number2 = parseInt(prompt("what is your second number:"))
-var result = add(number1, number2);
-alert(result);
-alert("subtraction")
-var subtract = function(number1, number2) {
-  return number1 - number2;
-};
-var number1 = parseInt(prompt("what is your first number:"))
-var number2 = parseInt(prompt("what is your second number"))
-var result = subtract(number1, number2);
-alert(result)
-alert("multiplication")
-var multiply = function(number1, number2){
-  return number1 + number2;
-};
-var number1 = parseInt(prompt("what is your first number:"))
-var number2 = parseInt(prompt("what is your second number:"))
-var result = multiply(number1, number2);
-alert(result)
-alert("division")
-var divide = function(number1, number2){
-  return number1 / number2;
-};
-var number1 = parseInt(prompt("what is your first number:"))
-var number2 = parseInt(prompt("what is your second number:"))
-var result = divide(number1, number2);
-alert(result)
+    var subtract = function(number1, number2) {
+      return number1 - number2;
+    };
+
+    var multiply = function(number1, number2) {
+      return number1 * number2;
+    };
+
+    var divide = function(number1, number2) {
+      return number1 / number2;
+    };
