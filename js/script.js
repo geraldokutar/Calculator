53ebdee926a2c8dbb4fb28097c8d1483359e20eb
geraldokutar <geraldpilk@gmail.com>
2018-08-02 20:28:25 +0300

    shownig the sum of the numbers user added in prompt

--- js/script.js
+++ js/script.js
@@ -1,24 +1,27 @@
-    var add = function(number1, number2) {
-      return number1 + number2;
-    };
+//Back end
 
-    var subtract = function(number1, number2) {
-      return number1 - number2;
-    };
+var add = function(number1, number2) {
+  return number1 + number2;
+};
 
-    var multiply = function(number1, number2) {
-      return number1 * number2;
-    };
+var subtract = function(number1, number2) {
+  return number1 - number2;
+};
 
-    var divide = function(number1, number2) {
-      return number1 / number2;
-    };
+var multiply = function(number1, number2) {
+  return number1 * number2;
+};
 
+var divide = function(number1, number2) {
+  return number1 / number2;
+};
+//front end
 
-      var number1 = parseInt($("#add1").val());
-      var number2 = parseInt($("$add2").val());
-      alert(add(number1,number2));
-
-
-$("#add1").val();
-$("#add2").val();
+$(document).ready(function() {
+  $("form#add").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#add1").val());
+    var number2 = parseInt($("#add2").val());
+    alert(add(number1, number2));
+  });
+});
